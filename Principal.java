a831ffe27634ce2ccbf5d0dd7072b6cf6aea61ca
Martin <mikanet@gmail.com>
2012-03-26 17:59:00 +0200

    Modificades les classes input per funcionar a windows.

--- Principal.java
+++ Principal.java
@@ -1,5 +1,7 @@
 package bicing;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 public class Principal {
@@ -16,17 +18,20 @@ public class Principal {
 	public static void main(String[] args) {
 
 		// Read data from user
-		System.out.println("[ IA-2012, Bicing  -  (Estaciones, bicicletas, furgonetas, demanda, seed) ]");
-		System.out.print("> ");
-
-		// Modificar classe input!!!
+		System.out.println("[ IA-2012, Bicing ]");
 
 		try {
-			ESTACIONES = Input.readInt();
-			BICICLETAS = Input.readInt();
-			FURGONETAS = Input.readInt();
-			DEMANDA = Input.readInt();
-			SEED = Input.readInt();
+
+			System.out.print("Estaciones: ");
+			ESTACIONES = readInt();
+			System.out.print("Bicicletas: ");
+			BICICLETAS = readInt();
+			System.out.print("Furgonetas: ");
+			FURGONETAS = readInt();
+			System.out.print("Demanda (1.equilibrada, 2.hora punta): ");
+			DEMANDA = readInt();
+			System.out.print("Seed: ");
+			SEED = readInt();
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -41,10 +46,34 @@ public class Principal {
 
 		ciudad.print();
 
+		// System.out.println("Estaciones: " + ESTACIONES + " Biciletas: " +
+		// BICICLETAS + " Furgonetas: " + FURGONETAS + " Demanda: " + DEMANDA +
+		// " Seed: " + SEED);
+
 		// end
 		System.out.println("-");
 		System.exit(0);
 
+	}
+
+	public static int readInt() {
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			return Integer.parseInt(stdin.readLine());
+		} catch (java.io.IOException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	public static double readDouble() {
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			return Double.parseDouble((stdin.readLine()));
+		} catch (java.io.IOException e) {
+			e.printStackTrace();
+		}
+		return 0;
 	}
 
 }
