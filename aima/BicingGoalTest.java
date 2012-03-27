d2bf72a43d1fb55485b3ae703e7f396f2fdda0e6
Martin <mikanet@gmail.com>
2012-03-27 13:21:47 +0200

    Afegides classes AIMA.

---
+++ aima/BicingGoalTest.java
@@ -0,0 +1,11 @@
+package bicing.aima;
+
+import aima.search.framework.GoalTest;
+
+public class BicingGoalTest implements GoalTest {
+
+	public boolean isGoalState(Object arg0) {
+		return false;
+	}
+
+}
---
+++ aima/BicingSuccessorFunctionSA.java
@@ -0,0 +1,19 @@
+package bicing.aima;
+
+import java.util.List;
+import java.util.Vector;
+
+import aima.search.framework.Successor;
+import aima.search.framework.SuccessorFunction;
+import bicing.Ciudad;
+
+public class BicingSuccessorFunctionSA implements SuccessorFunction {
+
+	public List<Successor> getSuccessors(Object aState) {
+		Vector<Successor> result = new Vector<Successor>();
+		Ciudad estadoCiudad = (Ciudad) aState;
+
+		return null;
+	}
+
+}
