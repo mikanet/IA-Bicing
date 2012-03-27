cd87e5750764c74b88e0f8528fe9a1817e71dbb4
Martin <mikanet@gmail.com>
2012-03-27 15:35:58 +0200

    Afegides estructures de Heuristics.

---
+++ aima/Bicing_HF_maxBeneficios.java
@@ -0,0 +1,10 @@
+package bicing.aima;
+
+import aima.search.framework.HeuristicFunction;
+
+public class Bicing_HF_maxBeneficios implements HeuristicFunction {
+
+	public double getHeuristicValue(Object ciudad) {
+		return 0.0;
+	}
+}
---
+++ aima/Bicing_HF_maxDistribucion.java
@@ -0,0 +1,11 @@
+package bicing.aima;
+
+import aima.search.framework.HeuristicFunction;
+
+public class Bicing_HF_maxDistribucion implements HeuristicFunction {
+
+	public double getHeuristicValue(Object ciudad) {
+		return 0.0;
+	}
+
+}
