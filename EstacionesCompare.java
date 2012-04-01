93f608d6269d37bcc11d91fed30826e8cc3bbb84
Vero <vero89mp@gmail.com>
2012-04-01 22:43:18 +0200

    Renombrada la función añadirTransporte a addTransporte. Empezada la función initEstrategiaElaborada(). Añadida la clase EstacionesCompare, auxiliar para la función initEstrategiaElaborada().

---
+++ EstacionesCompare.java
@@ -0,0 +1,25 @@
+package bicing;
+
+public class EstacionesCompare {
+	int origen;
+	int sobrantes;
+
+	public EstacionesCompare() {
+	}
+
+	public int getOrigen() {
+		return this.origen;
+	}
+
+	public void setOrigen(int origen) {
+		this.origen = origen;
+	}
+
+	public int getSobrantes() {
+		return this.sobrantes;
+	}
+
+	public void setSobrantes(int sobrantes) {
+		this.sobrantes = sobrantes;
+	}
+}
